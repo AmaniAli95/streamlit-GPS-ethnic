edf2ae87fa4166a45f649fbb4f3b79dca260ec72
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-01-16 12:14:02 +0800

    Update GPS-votes-simulation.py

--- GPS-votes-simulation.py
+++ GPS-votes-simulation.py
@@ -230,7 +230,6 @@ if st.button('Submit'):
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     credentials = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
     client = gspread.authorize(credentials)
-    st.write("read url")
     dfall = pd.DataFrame(all_data, index=[0])
     dfall["Total Vote Count Forecast"] = GPSvote
     dfall["Not Vote GPS"] = nonGPSvote
@@ -243,11 +242,11 @@ if st.button('Submit'):
     dfall.insert(2, "Parliament", level, True)
     dfall.insert(3, "District", d_name, True)
     dfall["Datetime"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    st.write(dfall)
 
     sheet = client.open_by_url(st.secrets["private_gsheets_url"])
     worksheet = sheet.get_worksheet(0)
     empty_row = worksheet.find("").row
-    worksheet.insert_rows(empty_row, {"values": dfall.values.tolist()})
+    st.write(empty_row)
+    worksheet.insert_rows(dfall.values.tolist(), row=empty_row)
     #worksheet.insert_rows(dfall.values.tolist(), row=2)
 
