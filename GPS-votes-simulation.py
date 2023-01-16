c05585ec53d30eb74787ac554b4370e3dcdaedb1
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-01-16 09:27:36 +0800

    Update GPS-votes-simulation.py

--- GPS-votes-simulation.py
+++ GPS-votes-simulation.py
@@ -230,9 +230,5 @@ def run_query(query):
     rows = rows.fetchall()
     return rows
 
-sheet_url = st.secrets["gcp_service_account"]
+sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
-# Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
