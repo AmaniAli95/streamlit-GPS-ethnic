4fde083ef9e943383468bfaf99043dc49e945f61
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-01-16 13:34:13 +0800

    Update GPS-votes-simulation.py

--- GPS-votes-simulation.py
+++ GPS-votes-simulation.py
@@ -229,7 +229,7 @@ st.button("Reset",on_click=_update_slider)
 description = st.text_input("Enter a description for the save file:")
 
 if st.button("Submit"):
-    scope = ["https://spreadsheets.google.com/feeds’, ‘https://www.googleapis.com/auth/drive"]
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     credentials = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
     client = gspread.authorize(credentials)
     dfall = pd.DataFrame(all_data, index=[0])
@@ -242,7 +242,7 @@ if st.button("Submit"):
     dfall.insert(0, "Description Save File", description)
     dfall.insert(1, "Parliament", level, True)
     dfall.insert(2, "District", d_name, True)
-    dfall["Datetime"] = datetime.datetime.now().strftime(“%Y-%m-%d %H:%M:%S”)
+    dfall["Datetime"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     sheet = client.open_by_url(st.secrets["private_gsheets_url"])
     worksheet = sheet.get_worksheet(0)
     worksheet.append_rows(dfall.values.tolist())
