f18eac0999f353a933936a83a9ef0c5e157f641e
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-01-17 20:56:43 +0800

    Update GPS-votes-simulation.py

--- GPS-votes-simulation.py
+++ GPS-votes-simulation.py
@@ -312,15 +312,16 @@ def _update_slider():
     for i, column_name in enumerate(renamed_columns.values()):
         key = f"slider_col3_{column_name}"
         st.session_state[key] = int(selected_row[f"{column_name} | Pct GPS Support Forecast"].values[0])
-    st.session_state["name"] = selected_row["Name Save Data"].values[0]
-    st.session_state["desc"] = selected_row["Description Save Data"].values[0]
+    st.write(selected_row["Name Save Data"].values[0])
+    #st.session_state["name"] = selected_row["Name Save Data"].values[0]
+    #st.session_state["desc"] = selected_row["Description Save Data"].values[0]
 
 loadBtn = st.sidebar.button("Load",on_click=_update_slider)
 if loadBtn:
-    st.session_state["name"] = f"{selected_row["Name Save Data"].values[0]}"
-    st.session_state["desc"] = f"{selected_row["Description Save Data"].values[0]}"
-    st.write(f"{selected_row["Name Save Data"].values[0]}")
-    st.write(f"{selected_row["Description Save Data"].values[0]}")
+    #st.session_state["name"] = f"{selected_row["Name Save Data"].values[0]}"
+    #st.session_state["desc"] = f"{selected_row["Description Save Data"].values[0]}"
+    #st.write(f"{selected_row["Name Save Data"].values[0]}")
+    #st.write(f"{selected_row["Description Save Data"].values[0]}")
     updateBtn = st.button("Update")
     if updateBtn:
         dfall = pd.DataFrame(all_data, index=[0])
