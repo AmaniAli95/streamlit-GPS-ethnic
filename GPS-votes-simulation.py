e64535bdc69d41474dc9834dfc442728b42e65e7
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-01-17 11:13:07 +0800

    Update GPS-votes-simulation.py

--- GPS-votes-simulation.py
+++ GPS-votes-simulation.py
@@ -239,8 +239,8 @@ elif chart_type == "Ethnic":
     st.markdown(result, unsafe_allow_html=True)
     soup = BeautifulSoup(result, 'html.parser')
     text_result = soup.h2.text   
-st.session_state[name] = d_name"-"datetime.datetime.now().strftime("YYYYMMDD")"-"datetime.datetime.now().strftime("HHMM")
-name = st.text_input("Enter a name for save data:",type=st.session_state[name])
+st.session_state["name"] = f"{d_name}-{datetime.datetime.now().strftime("%Y%m%d")}-{datetime.datetime.now().strftime("%H%M")}"
+name = st.text_input("Enter a name for save data:",type=st.session_state["name"])
 description = st.text_input("Enter a description for save data:")
 
 #submit btn
