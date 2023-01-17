ca2856b1892d6eb1557c408124b3021b64a73069
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-01-17 11:35:55 +0800

    Update GPS-votes-simulation.py

--- GPS-votes-simulation.py
+++ GPS-votes-simulation.py
@@ -55,13 +55,25 @@ if chart_type == "Ethnic":
     worksheet = sheet.get_worksheet(0)
     data = worksheet.get_all_values()
     df2 = pd.DataFrame(data[1:], columns=data[0])
-    selected_name = st.sidebar.selectbox('Recent Save Data:', df2["Name Save Data"].dropna().unique().tolist())
+    filtered_df2 = df2.loc[df2["District"] == d_name]
+    selectname_options = filtered_df2["Name Save Data"].dropna().unique().tolist()
+    if selectname_options:
+        selected_name = st.sidebar.selectbox('Recent Save Data:', selectname_options)
+    else:
+        selected_name = None
+    #selected_name = st.sidebar.selectbox('Recent Save Data:', df2["Name Save Data"].dropna().unique().tolist())
 #age
 elif chart_type == "Age":
     worksheet = sheet.get_worksheet(1)
     data = worksheet.get_all_values()
     df2 = pd.DataFrame(data[1:], columns=data[0])
-    selected_name = st.sidebar.selectbox('Recent Save Data:', df2["Name Save Data"].dropna().unique().tolist())
+    filtered_df2 = df2.loc[df2["District"] == d_name]
+    selectname_options = filtered_df2["Name Save Data"].dropna().unique().tolist()
+    if selectname_options:
+        selected_name = st.sidebar.selectbox('Recent Save Data:', selectname_options)
+    else:
+        selected_name = None
+    #selected_name = st.sidebar.selectbox('Recent Save Data:', df2["Name Save Data"].dropna().unique().tolist())
 
 #Number of Registered Voters
 def to_percentage(val):
