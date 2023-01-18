e9da539d81be2e33993410331fe3e86d51ffcb24
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-01-18 22:10:41 +0800

    Update GPS-votes-simulation.py

--- GPS-votes-simulation.py
+++ GPS-votes-simulation.py
@@ -292,7 +292,7 @@ else:
     name = st.text_input("Enter a name for save data:",value = st.session_state["name"])
     description = st.text_input("Enter a description for save data:", value = st.session_state["desc"])
     updateBtnDis = False
-    updateBtn = st.button("Update", disable = updateBtnDis)
+    updateBtn = st.button("Update", disabled = updateBtnDis)
     resetBtn = st.button("Reset",on_click=_reset_slider)
     #updateBtn
     if updateBtn:
