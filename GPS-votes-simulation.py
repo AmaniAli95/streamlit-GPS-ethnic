03443be0cd34a2b34c3683eb5cf0664231157b3e
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-01-18 20:30:25 +0800

    Update GPS-votes-simulation.py

--- GPS-votes-simulation.py
+++ GPS-votes-simulation.py
@@ -275,7 +275,7 @@ def _load_slider():
         st.session_state[key] = int(selected_row[f"{column_name} | Pct GPS Support Forecast"].values[0])
     st.session_state["name"] = selected_row["Name Save Data"].values[0]
     st.session_state["desc"] =  selected_row["Description Save Data"].values[0]
-    return st.session_state["name"], st.session_state["desc"],selected_row
+    return st.session_state["name"], st.session_state["desc"], selected_row
 loadBtn = st.sidebar.button("Load",on_click=_load_slider)  
 
 #check loadBtn
@@ -289,6 +289,9 @@ if not loadBtn:
     resetBtn = st.button("Reset",on_click=_reset_slider)
 else:
     st.write("2")
+    st.write(st.session_state["name"])
+    st.write(st.session_state["desc"])
+    st.write(selected_row)
     name = st.text_input("Enter a name for save data:",value = st.session_state["name"])
     description = st.text_input("Enter a description for save data:", value = st.session_state["desc"])
     updateBtn = st.button("Update", disabled=False)
