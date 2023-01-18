949597b51e7a61de9bc44a558c09bd1784905e92
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-01-18 22:53:32 +0800

    Update GPS-votes-simulation.py

--- GPS-votes-simulation.py
+++ GPS-votes-simulation.py
@@ -281,14 +281,14 @@ def _load_slider():
     return st.session_state["name"], st.session_state["desc"]
 loadBtn = st.sidebar.button("Load",on_click=_load_slider)  
 
+updateBtn_disabled = False
 #check loadBtn
 if not loadBtn and "name" not in st.session_state:
     st.session_state["name"] = f"{d_name}-{datetime.datetime.now(tz).strftime('%Y%m%d')}-{datetime.datetime.now(tz).strftime('%H%M')}"
     st.session_state["desc"] =  " "
     name = st.text_input("Enter a name for save data:",value = st.session_state["name"])
     description = st.text_input("Enter a description for save data:", value = st.session_state["desc"])
-    updateBtn_disabled = True
-    updateBtn = st.button("Update", disabled=updateBtn_disabled)
+    updateBtn = st.button("Update", disabled=True)
     resetBtn = st.button("Reset",on_click=_reset_slider)
 else:
     name = st.text_input("Enter a name for save data:",value = st.session_state["name"])
