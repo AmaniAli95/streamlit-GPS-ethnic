8f235e194295c38c7b7de442dd44a3e4f9943c5c
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-01-18 21:12:19 +0800

    Update GPS-votes-simulation.py

--- GPS-votes-simulation.py
+++ GPS-votes-simulation.py
@@ -283,7 +283,7 @@ dateCheck = datetime.datetime.now(tz).strftime("%H%M")
 st.write(dateCheck)
 
 #check loadBtn
-if not loadBtn and "name" not in st.session_state::
+if not loadBtn and "name" not in st.session_state:
     st.write("1")
     st.session_state["name"] = f"{d_name}-{datetime.datetime.now(tz).strftime('%Y%m%d')}-{datetime.datetime.now(tz).strftime('%H%M')}"
     st.session_state["desc"] =  " "
