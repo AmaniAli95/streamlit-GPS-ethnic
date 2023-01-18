26e79433567afcd8e10bd2cc6c10f95001de343b
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-01-18 20:51:58 +0800

    Update GPS-votes-simulation.py

--- GPS-votes-simulation.py
+++ GPS-votes-simulation.py
@@ -275,11 +275,15 @@ def _load_slider():
         st.session_state[key] = int(selected_row[f"{column_name} | Pct GPS Support Forecast"].values[0])
     st.session_state["name"] = selected_row["Name Save Data"].values[0]
     st.session_state["desc"] =  selected_row["Description Save Data"].values[0]
-    return st.session_state["name"], st.session_state["desc"], st.session_state["dname_index"]
+    return st.session_state["name"], st.session_state["desc"]
 loadBtn = st.sidebar.button("Load",on_click=_load_slider)  
 
+#checking
+dateCheck = datetime.datetime.now(tz).strftime("%H%M")
+st.write(dateCheck)
+
 #check loadBtn
-if not loadBtn:
+if not loadBtn and not st.session_state["name"]
     st.write("1")
     st.session_state["name"] = f"{d_name}-{datetime.datetime.now(tz).strftime('%Y%m%d')}-{datetime.datetime.now(tz).strftime('%H%M')}"
     st.session_state["desc"] =  " "
