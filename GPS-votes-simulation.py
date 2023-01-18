3d910e9cdd30e1a129dd22110bb00feceeda113f
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-01-18 12:12:00 +0800

    Update GPS-votes-simulation.py

--- GPS-votes-simulation.py
+++ GPS-votes-simulation.py
@@ -280,19 +280,11 @@ def _load_slider():
     return st.session_state["name"], st.session_state["desc"]
 loadBtn = st.sidebar.button("Load",on_click=_load_slider)
 
-#text_input
-st.session_state["name"] = f"{d_name}-{datetime.datetime.now(tz).strftime('%Y%m%d')}-{datetime.datetime.now(tz).strftime('%H%M')}"
-st.session_state["desc"] =  " "
-#name = st.text_input("Enter a name for save data:",value = st.session_state["name"])
-#description = st.text_input("Enter a description for save data:", value = st.session_state["desc"])
-#updateBtn = st.button("Update", disabled=True)
-#resetBtn = st.button("Reset",on_click=_reset_slider)
-
 #check loadBtn
-if not loadBtn and selected_name != st.session_state["name"]:
+if not loadBtn:
     st.write("1")
-    #st.session_state["name"] = f"{d_name}-{datetime.datetime.now(tz).strftime('%Y%m%d')}-{datetime.datetime.now(tz).strftime('%H%M')}"
-    #st.session_state["desc"] =  " "
+    st.session_state["name"] = f"{d_name}-{datetime.datetime.now(tz).strftime('%Y%m%d')}-{datetime.datetime.now(tz).strftime('%H%M')}"
+    st.session_state["desc"] =  " "
     name = st.text_input("Enter a name for save data:",value = st.session_state["name"])
     description = st.text_input("Enter a description for save data:", value = st.session_state["desc"])
     updateBtn = st.button("Update", disabled=True)
