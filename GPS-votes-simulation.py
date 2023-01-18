ebb651b0f9b4767e66fd6a0ebd68f3a7c3d2ff57
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-01-18 15:47:20 +0800

    Update GPS-votes-simulation.py

--- GPS-votes-simulation.py
+++ GPS-votes-simulation.py
@@ -275,20 +275,45 @@ def _load_slider():
         st.session_state[key] = int(selected_row[f"{column_name} | Pct GPS Support Forecast"].values[0])
     st.session_state["name"] = selected_row["Name Save Data"].values[0]
     st.session_state["desc"] =  selected_row["Description Save Data"].values[0]
-    return st.session_state["name"], st.session_state["desc"]
+    return st.session_state["name"], st.session_state["desc"],selected_row
 loadBtn = st.sidebar.button("Load",on_click=_load_slider)  
 
 #check loadBtn
-if not loadBtn and 'updateBtn' not in globals() and 'updateBtn' not in locals():
-    st.write("1")
-    st.session_state["name"] = f"{d_name}-{datetime.datetime.now(tz).strftime('%Y%m%d')}-{datetime.datetime.now(tz).strftime('%H%M')}"
-    st.session_state["desc"] =  " "
-    name = st.text_input("Enter a name for save data:",value = st.session_state["name"])
-    description = st.text_input("Enter a description for save data:", value = st.session_state["desc"])
-    updateBtn = st.button("Update", disabled=True)
-    resetBtn = st.button("Reset",on_click=_reset_slider)
+if not loadBtn:
+    if selected_row != st.session_state["name"]:
+        st.write("1")
+        st.session_state["name"] = f"{d_name}-{datetime.datetime.now(tz).strftime('%Y%m%d')}-{datetime.datetime.now(tz).strftime('%H%M')}"
+        st.session_state["desc"] =  " "
+        name = st.text_input("Enter a name for save data:",value = st.session_state["name"])
+        description = st.text_input("Enter a description for save data:", value = st.session_state["desc"])
+        updateBtn = st.button("Update", disabled=False)
+        resetBtn = st.button("Reset",on_click=_reset_slider)
+    else:
+        st.write("2")
+        name = st.text_input("Enter a name for save data:",value = st.session_state["name"])
+        description = st.text_input("Enter a description for save data:", value = st.session_state["desc"])
+        updateBtn = st.button("Update", disabled=True)
+        resetBtn = st.button("Reset",on_click=_reset_slider)
+        if updateBtn:
+            dfall = pd.DataFrame(all_data, index=[0])
+            dfall["Total Vote Count Forecast"] = GPSvote
+            dfall["Not Vote GPS"] = nonGPSvote
+            dfall["Total Voter"] = total.values
+            dfall["Simple Majority Votes"] = GPSwin
+            dfall["Two Third Winning"] = GPSwin23
+            dfall["Result"] = text_result
+            dfall.insert(0, "Name Save Data", name)
+            dfall.insert(1, "Description Save Data", description)
+            dfall.insert(2, "Parliament", level, True)
+            dfall.insert(3, "District", d_name, True)
+            dfall["Datetime"] = datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+            st.write("haha")
+            worksheet.update(dfall.to_dict("records"), 'Name Save Data = "{}"'.format(name))
+    if resetBtn:
+        updateBtn.disable()
+        
 else:
-    st.write("2")
+    st.write("3")
     name = st.text_input("Enter a name for save data:",value=st.session_state["name"])
     description = st.text_input("Enter a description for save data:",value=st.session_state["desc"])
     updateBtn = st.button("Update", disabled=False)
@@ -307,6 +332,7 @@ else:
         dfall.insert(2, "Parliament", level, True)
         dfall.insert(3, "District", d_name, True)
         dfall["Datetime"] = datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+        st.write("dada")
         worksheet.update(dfall.to_dict("records"), 'Name Save Data = "{}"'.format(name))
     if resetBtn:
         updateBtn.disable()
