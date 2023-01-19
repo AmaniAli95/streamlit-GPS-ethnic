4dba83d07f0e4474b6bef23e848cc9c164d40a3c
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-01-19 15:41:37 +0800

    Update GPS-votes-simulation.py

--- GPS-votes-simulation.py
+++ GPS-votes-simulation.py
@@ -266,6 +266,7 @@ def _reset_slider(updateBtn_disabled):
         st.session_state[key] = 70  
     st.session_state["name"] = f"{d_name}-{datetime.datetime.now(tz).strftime('%Y%m%d')}-{datetime.datetime.now(tz).strftime('%H%M')}"
     st.session_state["desc"] = " "
+    return updateBtn_disabled
 
 def _load_slider():
     selected_row = df2.loc[df2["Name Save Data"] == selected_name]
