fc7db5faad4c0c31173584b3e5be14bc72fc09a8
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-02-02 14:52:39 +0800

    Update GPS-ethnic.py

--- GPS-ethnic.py
+++ GPS-ethnic.py
@@ -45,25 +45,25 @@ if check_password():
     st.write("Here goes your normal Streamlit app...")
     st.button("Click me")
 
-folder_path = "https://api.github.com/repos/AmaniAli95/streamlit-GPS-ethnic/contents/scoresheets-ge15-pdf"
-response = requests.get(folder_path)
+    folder_path = "https://api.github.com/repos/AmaniAli95/streamlit-GPS-ethnic/contents/scoresheets-ge15-pdf"
+    response = requests.get(folder_path)
 
-pdf_files = [f for f in response.json() if f["name"].endswith(".pdf")]
-st.sidebar.header("File List")
-filename = st.sidebar.radio("Select a file:", [pdf_file["name"] for pdf_file in pdf_files])
+    pdf_files = [f for f in response.json() if f["name"].endswith(".pdf")]
+    st.sidebar.header("File List")
+    filename = st.sidebar.radio("Select a file:", [pdf_file["name"] for pdf_file in pdf_files])
 
-def show_pdf(file_path):
-    with open(file_path,"rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    #pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-    pdf_display = f'<div style="position:relative; height:800px; width:800px;"><iframe src="data:application/pdf;base64,{base64_pdf}" style="position:absolute; height:100%; width:100%; border:none;"></iframe></div>'
+    def show_pdf(file_path):
+        with open(file_path,"rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        #pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+        pdf_display = f'<div style="position:relative; height:800px; width:800px;"><iframe src="data:application/pdf;base64,{base64_pdf}" style="position:absolute; height:100%; width:100%; border:none;"></iframe></div>'
 
-    st.markdown(pdf_display, unsafe_allow_html=True)
+        st.markdown(pdf_display, unsafe_allow_html=True)
 
-pdf_url = [pdf_file["download_url"] for pdf_file in pdf_files if pdf_file["name"] == filename][0]
-st.write("Content of the PDF file:")
-response = requests.get(pdf_url)
-with open("temp.pdf", "wb") as f:
-    f.write(response.content)
-show_pdf("temp.pdf")
+    pdf_url = [pdf_file["download_url"] for pdf_file in pdf_files if pdf_file["name"] == filename][0]
+    st.write("Content of the PDF file:")
+    response = requests.get(pdf_url)
+    with open("temp.pdf", "wb") as f:
+        f.write(response.content)
+    show_pdf("temp.pdf")
 
