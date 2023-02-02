d7d8d5bd77dcd74120ca17cf32620eb806d3690d
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-02-02 12:14:31 +0800

    Update GPS-ethnic.py

--- GPS-ethnic.py
+++ GPS-ethnic.py
@@ -5,8 +5,7 @@ st.set_page_config(layout="wide")
 st.title("Scoresheet GE15")
 
 folder_path = "https://github.com/AmaniAli95/streamlit-GPS-ethnic/tree/main/scoresheets-ge15-pdf"
-response = requests.get(folder_path)
-pdf_files = [f for f in response.json() if f["name"].endswith(".pdf")]
+filenames = [f for f in folder_path if f.endswith(".pdf")]
 
 st.header("File List")
 for filename in filenames:
