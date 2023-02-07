dead4366703f8984ce7c6e22127f5362b7771ca2
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-02-07 10:10:38 +0800

    Update GPS-ethnic.py

--- GPS-ethnic.py
+++ GPS-ethnic.py
@@ -63,8 +63,8 @@ if check_password():
     response = requests.get(pdf_url)
     with open("temp.pdf", "wb") as f:
         f.write(response.content)
-    show_pdf("temp.pdf")
-    st.write("kk")
+#    show_pdf("temp.pdf")
+
     pdf_file = 'temp.pdf'
     IFrame(pdf_file, width=800, height=600, align='center')
 
